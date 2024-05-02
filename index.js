e6428c7376e9cf22de9a2161ff8a40df0d225d26
Samuel Gracia <macedosam55@gmail.com>
2024-05-02 12:06:40 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,39 @@
-const arrayVazia = [, , , ];
+const arrayVazia = [, 3, , 5, , , , 90];
 
-// console.log(arrayVazia[0]);
-// console.log(arrayVazia[1]);
-// console.log(arrayVazia[2]);
-// console.log(arrayVazia.length);
+/* console.log(arrayVazia[0]);
+ console.log(arrayVazia[1]);
+console.log(arrayVazia[2]);
+console.log(arrayVazia.length); */
 
-console.log(arrayVazia.length);
+/* console.log(arrayVazia.length);
 arrayVazia.push(50);
 console.log(arrayVazia);
+console.log(arrayVazia.length); */
+
+/* const lista = [7, 13, 'JavaScript', 'Alura', true];
+lista.push(false);
+console.log(lista.length); // 6 */
+
+/*const jantarDeHoje = ["burguer", "hot dog", "pizza"];
+
+jantarDeHoje.pop()
+jantarDeHoje.pop()
+
+jantarDeHoje.push('ovo frito')
+jantarDeHoje.push('salada')
+jantarDeHoje.push('maçã')
+
+console.log(jantarDeHoje) */
+
 console.log(arrayVazia.length);
+
+const array5Elementos = [1, 2, 3, 4, 5]
+console.log(`Meu array antes da alteração: ${array5Elementos}`);
+array5Elementos[0] = 20
+console.log(`Meu array depois da alteração: ${array5Elementos}`);
+
+const meuArray = []
+meuArray.push(90)
+meuArray.push(20)
+meuArray.push(60)
+console.log(meuArray);
