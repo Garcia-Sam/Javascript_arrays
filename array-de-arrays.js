d148eee76f568f3b371cfff99cb02243b5d27fbf
Samuel Gracia <macedosam55@gmail.com>
2024-05-06 11:55:59 -0300

    Update array-de-arrays.js

--- array-de-arrays.js
+++ array-de-arrays.js
@@ -5,6 +5,7 @@ const medias = [10, 8, 7.5, 9];
 const lista = [alunos, medias];
 
 // console.log(lista); // exibe um único array, contendo os dois elementos (alunos, medias)
+
 console.log(
     `A aluna da posição 1 da lista é ${lista[0][1]}.
 A nota desta aluna é ${lista[1][1]}.`
