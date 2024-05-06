eac4ed3655d25aa576955c26d32579734c148a0b
Samuel Gracia <macedosam55@gmail.com>
2024-05-06 11:46:48 -0300

    Update juntar-salas.js

--- juntar-salas.js
+++ juntar-salas.js
@@ -1,6 +1,6 @@
 const salaJS = ['Evaldo', 'Camis', 'Mari'];
 const salaPython = ['Ju', 'Leo', 'Raquel'];
 
-const salasUnificadas = salaJS.concat(salaPython);
+const salasUnificadas = salaJS.concat(salaPython); // não altera os arrays existente, retorana um novo array
 
 console.log(salasUnificadas);
