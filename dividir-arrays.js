f5c3e6a9145c803f88056a20cb167fcf6fd457b0
Samuel Gracia <macedosam55@gmail.com>
2024-05-06 11:23:31 -0300

    Create dividir-arrays.js

---
+++ dividir-arrays.js
@@ -0,0 +1,1 @@
+const listaEstudantes = ['João', 'Juliana', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Guilherme', 'Aline', 'Fabiana', 'André', 'Carlos', 'Paulo', 'Bia', 'Vivian', 'Isabela', 'Vinícius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
