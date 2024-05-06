a1cc805e4bcfd5ec456f94f2078062884270146f
Samuel Gracia <macedosam55@gmail.com>
2024-05-06 11:41:28 -0300

    Update atualizar-lista.js

--- atualizar-lista.js
+++ atualizar-lista.js
@@ -3,3 +3,11 @@ const listaEstudantes = ['João', 'Ana', 'Caio', 'Lara', 'Marjorie', 'Leo'];
 listaEstudantes.splice(1, 2, 'Rodrigo'); // splice altera o próprio array
 
 console.log(listaEstudantes);
+
+animaisDoAquario = ['baleia', 'polvo', 'golfinho', 'tubarão']
+
+animaisDoAquario.splice(1, 0, 'sardinha')
+animaisDoAquario.splice(3, 2, 'atum')
+
+console.log(animaisDoAquario)
+
