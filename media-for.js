985cca707cac1b47244c27c632e1b61b53d129ed
Samuel Gracia <macedosam55@gmail.com>
2024-05-08 12:10:56 -0300

    Update media-for.js

--- media-for.js
+++ media-for.js
@@ -1,4 +1,4 @@
-const notas = [10, 6.5, 8, 7.5];
+/* const notas = [10, 6.5, 8, 7.5];
 let somaDasNotas = 0
 
 for (let i = 0; i < notas.length; i++) {
@@ -7,4 +7,21 @@ for (let i = 0; i < notas.length; i++) {
 }
 const media = somaDasNotas / notas.length
 
-console.log(`A média das notas é ${media}`);
+console.log(`A média das notas é ${media}`); */
+
+const notas1 = [10 , 6.5, 8 ,7.5]
+const notas2 = [9  , 6  , 6]
+const notas3 = [8.5, 9.5]
+ 
+const notasGerais = [notas1,notas2,notas3]
+ 
+let media = 0
+ 
+for (let i = 0; i < notasGerais.length; i++) {
+  for (let j = 0; j < notasGerais[i].length; j++) {
+    media += notasGerais[i][j]/notasGerais[i].length;
+  }
+}
+ 
+console.log(`A média geral é ${media}`);
+
