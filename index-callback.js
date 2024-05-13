95def4518379f213fae3286f8ecd6027f0e2e3b2
Samuel Gracia <macedosam55@gmail.com>
2024-05-13 11:32:23 -0300

    Update index-callback.js

--- index-callback.js
+++ index-callback.js
@@ -1,1 +1,9 @@
 const arrayNums = [1, 2, 3, 4];
+
+function multiplicarPorDez (num) {
+    return num * 10;
+}
+
+const arrayMultiplicada = arrayNums.map(multiplicarPorDez);
+
+console.log(arrayMultiplicada);
