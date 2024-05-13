9cb8a066248e3d01a21a080243a98e1e2e523b93
Samuel Gracia <macedosam55@gmail.com>
2024-05-13 11:22:19 -0300

    Update map-string.js

--- map-string.js
+++ map-string.js
@@ -1,5 +1,14 @@
-const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
+/* const nomes = ["ana Julia", "Caio vinicius", "BIA silva"];
 
 const nomesPadronizados = nomes.map((nome) => nome.toUpperCase());
 
-console.log(nomesPadronizados);
+console.log(nomesPadronizados); */
+
+const nome = "Alura";
+let nomeMaiusculas = "";
+
+for (let i = 0; i < nome.length; i++) {
+    nomeMaiusculas += nome[i].toUpperCase();
+}
+
+console.log(nomeMaiusculas);
