2491c50aff17a361bdba113572d089edcf9ecbc0
Samuel Gracia <macedosam55@gmail.com>
2024-05-14 11:10:16 -0300

    Update filtrar.js

--- filtrar.js
+++ filtrar.js
@@ -1,3 +1,9 @@
 const alunos = ['Ana', 'Marcos', 'Maria', 'Mauro'];
 const medias = [7, 4.5, 8, 7.5];
 
+const tamanhoNome = alunos.filter((_, indice) => {  // o métoodo filter retorna o resultado da avaliação
+    return medias[indice] < 7;
+
+})
+
+console.log(tamanhoNome); // filter sempre retorna um array, assim como o map
